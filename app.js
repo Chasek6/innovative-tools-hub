18de522fdeaebd5a7c78983ca79b796d7503b31c
Chase Stratton <chasestratton@Chases-MacBook-Pro.local>
2024-06-19 17:44:48 -0400

    first new push

---
+++ app.js
@@ -0,0 +1,11 @@
+// Bootstrap JS components (already bundled with Bootstrap)
+import 'bootstrap';
+
+// Foundation JS components
+import 'foundation-sites/dist/js/foundation.min.js';
+
+// Materialize JS components
+import 'materialize-css/dist/js/materialize.min.js';
+
+// Custom JavaScript logic
+// Your custom scripts and initialization code
